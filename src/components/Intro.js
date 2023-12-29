1a871da0aa8c54292d346f29ede86adbe53b4a08
faisalbaqirt <faishalb0@gmail.com>
2023-12-29 21:48:11 +0700

    add intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,16 @@
+const Intro = () => {
+  return (
+    <>
+      <div className="container-fluid intro-container">
+        <div className="intro-section">
+          <div className="intro-title mb-5">Contact Web App.</div>
+          <div className="intro-text mb-3">
+            Welcome to Contact Web App
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Intro;
