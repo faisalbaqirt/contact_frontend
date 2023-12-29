d136543255edfa472cffdb04706fe7c5d86a7d63
faisalbaqirt <faishalb0@gmail.com>
2023-12-29 21:18:44 +0700

    using modal dialog in register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { registerUser } from "../redux/actions/authAction";
+import ModalDialog from "./ModalDialog";
 
 const Register = () => {
   const dispatch = useDispatch();
@@ -9,9 +11,26 @@ const Register = () => {
     username: "",
     password: "",
   });
+  const [modalShow, setModalShow] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    title: "",
+    message: "",
+  });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (modalShow === false && modalContent.title === "Success") {
+      navigate("/login");
+    }
+  }, [modalShow, modalContent, navigate]);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleModalClose = () => {
+    setModalShow(false);
   };
 
   const handleSubmit = async (e) => {
@@ -21,10 +40,18 @@ const Register = () => {
       const response = await dispatch(registerUser(formData));
       if (response.payload.message === "This username is already registered") {
         setIsLoading(false)
-        alert(response.payload.message);
+        setModalContent({
+          title: "Error",
+          message: response.payload.message,
+        });
+        setModalShow(true);
       } else {
         setIsLoading(false)
-        alert("Successfully created an account");
+        setModalContent({
+          title: "Success",
+          message: "Successfully created an account",
+        });
+        setModalShow(true);
       }
     } catch (error) {
       throw error;
@@ -77,6 +104,12 @@ const Register = () => {
           Already have an account? <a href="/login">Login</a>
         </div>
       </div>
+      <ModalDialog
+        show={modalShow}
+        onHide={handleModalClose}
+        title={modalContent.title}
+        message={modalContent.message}
+      />
     </>
   );
 };
