f7ddfa4a120ab6917a6b418f2a8ca5fd4eb3383c
faisalbaqirt <faishalb0@gmail.com>
2023-12-29 21:44:01 +0700

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="container-fluid footer-section text-center">
+        <p>&copy; 2023 Faisal Baqir Tahmidi</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
