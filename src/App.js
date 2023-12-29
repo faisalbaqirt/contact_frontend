14fdbd057821ee627d6458b4985a3f0872733c03
faisalbaqirt <faishalb0@gmail.com>
2023-12-29 21:56:33 +0700

    add route for home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Home from "./pages/Home";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import "./App.css";
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
+        <Route path="/" element={<Home />} />
       </BrowserRouter>
     </Provider>
   );
