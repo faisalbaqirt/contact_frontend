e1df75977ec6a92f4e9b0e2db422c97eca664e7b
faisalbaqirt <faishalb0@gmail.com>
2023-12-29 20:42:02 +0700

    add route for login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Register from "./components/Register";
+import Login from "./components/Login";
 import "./App.css";
 
 const App = () => {
@@ -10,6 +11,9 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register />} />
+        </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </Provider>
