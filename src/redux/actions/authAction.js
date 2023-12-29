f1064bc5a3aa8093f157f193b4a4b02d987a01dc
faisalbaqirt <faishalb0@gmail.com>
2023-12-29 19:56:05 +0700

    add thunks for auth

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // Action Types
 export const REGISTER = "REGISTER";
 export const LOGIN = "LOGIN";
@@ -12,3 +14,22 @@ export const login = (user) => ({
   type: LOGIN,
   payload: user,
 });
+
+// Thunks
+export const registerUser = (userData) => async (dispatch) => {
+  try {
+    const response = await axios.post("http://localhost:5000/api/v1/auth/register", userData);
+    return dispatch(register(response.data));
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const loginUser = (userData) => async (dispatch) => {
+  try {
+    const response = await axios.post("http://localhost:5000/api/v1/auth/login", userData);
+    return dispatch(login(response.data));
+  } catch (error) {
+    throw error;
+  }
+};
