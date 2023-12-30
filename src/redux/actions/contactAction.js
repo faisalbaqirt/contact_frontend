210f2049f76e6febb9d3229d56231543b60a8eeb
faisalbaqirt <faishalb0@gmail.com>
2023-12-30 21:20:22 +0700

    add action types for contact

---
+++ src/redux/actions/contactAction.js
@@ -0,0 +1,6 @@
+// Action Types
+export const GET_ALLCONTACTS = "GET_ALLCONTACTS";
+export const GET_CONTACT = "GET_CONTACT";
+export const CREATE_CONTACT = "CREATE_CONTACT";
+export const UPDATE_CONTACT = "CONTACT";
+export const DELETE_CONTACT = "DELETE_CONTACT";
