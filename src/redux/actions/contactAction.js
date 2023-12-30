9dc4853989f9224ecd883e14c39706b574e21994
faisalbaqirt <faishalb0@gmail.com>
2023-12-30 21:21:20 +0700

    add action creators for contact

--- src/redux/actions/contactAction.js
+++ src/redux/actions/contactAction.js
@@ -4,3 +4,27 @@ export const GET_CONTACT = "GET_CONTACT";
 export const CREATE_CONTACT = "CREATE_CONTACT";
 export const UPDATE_CONTACT = "CONTACT";
 export const DELETE_CONTACT = "DELETE_CONTACT";
+
+// Action Creators
+export const getAllContacts = (contact) => ({
+  type: GET_ALLCONTACTS,
+  payload: contact,
+});
+
+export const getContact = (contact) => ({
+  type: GET_CONTACT,
+  payload: contact,
+});
+
+export const createContact = () => ({
+  type: CREATE_CONTACT,
+});
+
+export const updateContact = () => ({
+  type: UPDATE_CONTACT,
+});
+
+export const deleteContact = (contactId) => ({
+  type: DELETE_CONTACT,
+  payload: contactId,
+});
