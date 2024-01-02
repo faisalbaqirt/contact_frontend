c83baa33b49cbc24c7588aabf1fec02d34affa67
faisalbaqirt <faishalb0@gmail.com>
2024-01-02 19:49:30 +0700

    add action creators for label

--- src/redux/actions/labelAction.js
+++ src/redux/actions/labelAction.js
@@ -2,3 +2,18 @@
 export const GET_ALLLABELS = "GET_ALLLABELS";
 export const CREATE_LABEL = "CREATE_LABEL";
 export const DELETE_LABEL = "DELETE_LABEL";
+
+// Action Creators
+export const getAllLabels = (label) => ({
+  type: GET_ALLLABELS,
+  payload: label,
+});
+
+export const createLabel = () => ({
+  type: CREATE_LABEL,
+});
+
+export const deleteLabel = (labelId) => ({
+  type: DELETE_LABEL,
+  payload: labelId,
+});
