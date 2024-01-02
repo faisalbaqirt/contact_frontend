289897f235de52d35f0b88c1fa0e4c839954fd93
faisalbaqirt <faishalb0@gmail.com>
2024-01-02 22:11:03 +0700

    add label form component

---
+++ src/components/LabelForm.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createNewLabel } from "../redux/actions/labelAction";
+import { addLabelToContact } from "../redux/actions/contactAction";
+import { FaTags } from "react-icons/fa6";
+
+const ContactForm = ({ isOpen, onClose, contactId }) => {
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const [newLabel, setNewLabel] = useState("");
+
+  const handleSave = () => {
+    setIsLoading(true);
+    if (contactId) {
+      dispatch(addLabelToContact(contactId, newLabel));
+      dispatch(createNewLabel(newLabel));
+    } else {
+      dispatch(createNewLabel(newLabel));
+    }
+    setIsLoading(false);
+    setNewLabel("");
+    onClose();
+  };
+
+  return (
+    <div
+      className={`modal ${isOpen ? "open" : ""}`}
+      tabIndex="-1"
+      role="dialog"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          {isLoading && (
+            <div className="loader-container">
+              <div className="spinner-border text-light" role="status"></div>
+            </div>
+          )}
+          <div className="modal-header">
+            <h5 className="modal-title">New Label</h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={onClose}
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div className="mb-3 row">
+              <div className="col-auto">
+                <FaTags />
+              </div>
+              <div className="col-auto">
+                <input
+                  type="text"
+                  value={newLabel}
+                  onChange={(e) => setNewLabel(e.target.value)}
+                  placeholder="New Label"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button className="btn btn-primary" onClick={handleSave}>
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
