36e97f878c72d5f6db978a7bd358dcb0e2882a3f
faisalbaqirt <faishalb0@gmail.com>
2024-01-02 21:25:31 +0700

    handle new contact

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,20 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAllContactsByUser } from "../redux/actions/contactAction";
+import { FaPlus } from "react-icons/fa6";
+import ContactForm from "./ContactForm";
 
 function ContactList() {
   const dispatch = useDispatch();
   const contacts = useSelector((state) => state.contact.contactlist);
+  const [isNewContactModalOpen, setIsNewContactModalOpen] = useState(false);
 
   useEffect(() => {
     dispatch(getAllContactsByUser());
   }, [dispatch]);
 
+  const handleNewContact = () => {
+    setIsNewContactModalOpen(true);
+  };
+
   return (
     <>
       <div>
         <h2 className="text-center m-3">Contact List</h2>
+        <div className="mb-3 ms-3">
+          <button className="btn btn-primary" onClick={handleNewContact}>
+            <FaPlus /> New Contact
+          </button>
+        </div>
         <div className="contact-list">
           <div className="contact-header row">
             <div className="header-item col">Name</div>
@@ -34,6 +46,10 @@ function ContactList() {
           ))}
         </div>
       </div>
+      <ContactForm
+        isOpen={isNewContactModalOpen}
+        onClose={() => setIsNewContactModalOpen(false)}
+      />
     </>
   );
 }
