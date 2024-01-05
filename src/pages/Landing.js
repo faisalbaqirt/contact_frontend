0e3e901434ffde46f24af3ed047d7d0eebd56330
faisalbaqirt <faishalb0@gmail.com>
2024-01-05 21:03:08 +0700

    add landing page

---
+++ src/pages/Landing.js
@@ -0,0 +1,15 @@
+import Intro from "../components/Intro";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <Intro />
+      <Footer />
+    </>
+  );
+};
+
+export default Landing;
