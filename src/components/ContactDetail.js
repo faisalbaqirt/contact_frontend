29f4f6b27a4f4b700095560f22c09405846ba612
faisalbaqirt <faishalb0@gmail.com>
2024-01-12 22:03:42 +0700

    show photo and handle update photo

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -5,6 +5,7 @@ import {
   getContactById,
   deleteContactById,
   updateContactStatus,
+  updateContactPhoto,
   addLabelToContact,
   removeLabelFromContact,
 } from "../redux/actions/contactAction";
@@ -19,6 +20,7 @@ import {
   FaPlus,
   FaRegStar,
   FaStar,
+  FaPen,
 } from "react-icons/fa6";
 import { PiTagSimpleBold } from "react-icons/pi";
 import ContactForm from "./ContactForm";
@@ -32,6 +34,9 @@ function ContactDetail() {
   const [isAddingLabel, setIsAddingLabel] = useState(false);
   const [isEditContactModal, setIsEditContactModal] = useState(false);
   const [selectedLabel, setSelectedLabel] = useState("");
+  const [photo, setPhoto] = useState(null);
+  const [photoPreview, setPhotoPreview] = useState(null);
+  const [isPhotoModalOpen, setIsPhotoModalOpen] = useState(false);
 
   useEffect(() => {
     dispatch(getContactById(contact_id));
@@ -44,6 +49,30 @@ function ContactDetail() {
 
   const handleToggleStatusFavorite = (id, newStatus) => {
     dispatch(updateContactStatus(id, newStatus));
+  };
+
+  const handleOpenPhotoModal = () => {
+    setIsPhotoModalOpen(true);
+  };
+
+  const handleClosePhotoModal = () => {
+    setIsPhotoModalOpen(false);
+    setPhoto(null);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setPhoto(file);
+      setPhotoPreview(URL.createObjectURL(file));
+    }
+  };
+
+  const handleUpdatePhoto = () => {
+    if (photo) {
+      dispatch(updateContactPhoto(contact_id, photo));
+    }
+    handleClosePhotoModal();
   };
 
   const navigate = useNavigate();
@@ -110,7 +139,25 @@ function ContactDetail() {
                 </div>
               </div>
             </div>
-            <div className="detail-name mt-3">{contact.name}</div>
+            <div className="row detail-header">
+              <div className="photo-col col-md-4 mt-3">
+                <img
+                  src={
+                    contact?.photo ||
+                    "https://res.cloudinary.com/dxgjnu4h8/image/upload/v1698433031/users/profile_ccs4ks.jpg"
+                  }
+                  className="contact-photo"
+                  alt=""
+                />
+                <div
+                  className="photo-action btn btn-outline-secondary"
+                  onClick={handleOpenPhotoModal}
+                >
+                  {contact.photo ? <FaPen /> : <FaPlus />}
+                </div>
+              </div>
+              <div className="detail-name col-auto m-3">{contact.name}</div>
+            </div>
             <div className="labels">
               {contact.labels.map((label, index) => (
                 <div key={index} className="label-item">
@@ -192,7 +239,7 @@ function ContactDetail() {
                 </div>
               </div>
             </div>
-            <div className="detail-col col-md-4 mt-3">
+            <div className="detail-col col-md-6 mt-3">
               <div className="item">
                 {" "}
                 <FaEnvelope /> {contact.email}
@@ -204,6 +251,48 @@ function ContactDetail() {
               <div className="item">
                 {" "}
                 <FaLocationDot /> {contact.address}
+              </div>
+            </div>
+            <div className={`modal ${isPhotoModalOpen ? "open" : ""}`}>
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">
+                      {contact.photo ? "Edit Photo" : "Add Photo"}
+                    </h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                      onClick={handleClosePhotoModal}
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <div className="photo-preview">
+                      <img
+                        className=""
+                        alt=""
+                        src={` ${
+                          photoPreview
+                            ? photoPreview
+                            : contact.photo ||
+                              "https://res.cloudinary.com/dxgjnu4h8/image/upload/v1698433031/users/profile_ccs4ks.jpg"
+                        }
+                        `}
+                      />
+                    </div>
+                    <input type="file" onChange={handleFileChange} />
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      className="btn btn-primary"
+                      onClick={handleUpdatePhoto}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
